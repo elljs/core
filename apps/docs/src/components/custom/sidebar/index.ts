3077216e8e3bfc8ba3e6481f2445e0ee26a4d8a1
RoyLin <18770221825@163.com>
2024-10-27 12:37:59 +0800

    feat: update

--- apps/docs/src/components/custom/sidebar/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./sidebar";
-export * from './sidebar-content';
-export * from './sidebar-footer';
-export * from './sidebar-group';
-export * from "./sidebar-header";
-export * from './sidebar-menu';
-export * from "./sidebar-trigger";
