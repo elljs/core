153faa6945f00fa31d19329cfea39d15c6ec3a95
RoyLin <18770221825@163.com>
2024-11-01 16:22:33 +0800

    feat: update

--- apps/admin/src/index.tsx
+++ apps/admin/src/index.tsx
@@ -1,8 +1,8 @@
+import { ModalProvider } from "@/components/custom/modal-provider";
 import { ThemeProvider } from "@/components/custom/theme-provider";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import { ModalProvider } from "./components/custom/modal-provider";
 import router from "./router";
 
 import "./index.css";
